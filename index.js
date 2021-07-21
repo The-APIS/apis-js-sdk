321364bb8547d15006098a3d81f80fd828218853
Arpit Shukla <arpitnshukla@gmail.com>
2021-07-21 06:48:33 +0530

    Updated Index

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 import Compound from "./src/compound";
 import ERC20 from "./src/erc20";
-export {ERC20, Compound}
+import ERC721 from "./src/erc721";
+import ERC1155 from "./src/erc1155";
+
+export {ERC20, ERC721, ERC1155, Compound}
