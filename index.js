8f65230f22afab1f041113534831b3c8b26c7a2f
Shain Lafazan <srslafazan@gmail.com>
2021-08-09 22:20:28 -0700

    Added command test:dist to test the dist in the browser

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ import ERC20 from './src/erc20'
 import ERC721 from './src/erc721'
 import ERC1155 from './src/erc1155'
 
+const APIS = { ERC20, ERC721, ERC1155, Compound }
+
+window.APIS = global.APIS = APIS
+
 export { ERC20, ERC721, ERC1155, Compound }
