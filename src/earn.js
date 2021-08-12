72a2791710e714713cd8119588f5b7512eee7065
Arpit Shukla <arpitnshukla@gmail.com>
2021-08-12 09:53:47 +0530

    Added log

--- src/earn.js
+++ src/earn.js
@@ -12,8 +12,8 @@ export default class Earn {
   async init() {
     // Get network provider and web3 instance.
     this.web3 = await getWeb3()
-    console.log('Received web3: ')
-    console.log(this.web3)
+    log.log('Received web3: ')
+    log.log(this.web3)
 
     // Use web3 to get the user's accounts.
     this.accounts = await this.web3.eth.getAccounts()
@@ -23,14 +23,14 @@ export default class Earn {
 
     // Get the chain ID
     this.chainId = await this.web3.eth.getChainId()
-    console.log('chain id is ' + this.chainId)
+    log.log('chain id is ' + this.chainId)
 
     // Fetch data from json file (cERC20,cETH,comptroller) as per the newtwork id
     this.network = Data.Addresses.find((network) => {
       return network.id === this.networkId && network.chainId === this.chainId
     })
-    console.log('fetching network successful: ')
-    console.log(this.network)
+    log.log('fetching network successful: ')
+    log.log(this.network)
     this.yERC20 = this.network.yERC20
     this.yERC20.forEach((object) => {
       // object.instance = new this.web3.eth.Contract(yDAI, object.address)
@@ -52,8 +52,8 @@ export default class Earn {
           break
       }
     })
-    console.log('Instanciated')
-    console.log(this.yERC20)
+    log.log('Instanciated')
+    log.log(this.yERC20)
     this.ERC20 = this.network.ERC20
     this.ERC20 = this.ERC20.map((object) => ({
       ...object,
@@ -73,7 +73,7 @@ export default class Earn {
       return ERC20.name === tokenName.substr(1)
     })
     await token.instance.methods.approve(ytoken.address, amount).send({ from: this.accounts[0] })
-    await ytoken.instance.methods.deposit(amount).send({ from: this.accounts[0] })
+    return ytoken.instance.methods.deposit(amount).send({ from: this.accounts[0] })
   }
 
   // amount of yToken balance
@@ -81,7 +81,7 @@ export default class Earn {
     let ytoken = this.yERC20.find((yERC20) => {
       return yERC20.name === tokenName
     })
-    await ytoken.instance.methods.withdraw(amount).send({ from: this.accounts[0] })
+    return ytoken.instance.methods.withdraw(amount).send({ from: this.accounts[0] })
   }
 
   async getTokenBalance(tokenName) {
@@ -89,8 +89,6 @@ export default class Earn {
       return yERC20.name === tokenName
     })
     let price = await ytoken.instance.methods.getPricePerFullShare().call()
-    console.log('current conversion rate of ' + ytoken.name + ' is')
-    console.log(price)
     let balanceShares = await ytoken.instance.methods.balanceOf(this.accounts[0]).call()
     return balanceShares * price
   }
@@ -99,8 +97,6 @@ export default class Earn {
     let token = this.yERC20.find((yERC20) => {
       return yERC20.name === tokenName
     })
-    console.log('token found')
-    console.log(token)
     return token.instance.methods.balanceOf(this.accounts[0]).call()
   }
 }
